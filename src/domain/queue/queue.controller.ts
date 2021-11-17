7f01065e1472bd70b57acb6db8cdd5b33748cbb9
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-17 09:44:22 +0300

    added grpc url

--- src/domain/queue/queue.controller.ts
+++ src/domain/queue/queue.controller.ts
@@ -41,6 +41,7 @@ export class QueueController implements OnModuleInit {
   @Client({
     transport: Transport.GRPC,
     options: {
+      url: 'ec2-18-217-138-210.us-east-2.compute.amazonaws.com',
       package: 'lab',
       protoPath: join(__dirname, '../../../grpc/grpc.proto'),
     },
